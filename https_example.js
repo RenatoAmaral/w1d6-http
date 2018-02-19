b4d81e361d45fb6405c372285272c5fd13367f95
Renato Amaral <srkimbo@gmail.com>
2018-02-19 16:10:24 +0000

    Final steps added to Https_examples.js

---
+++ https_example.js
@@ -0,0 +1,41 @@
+var https = require('https');
+
+console.log('I did it!');
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+// called by https when the request is made.
+var callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
+
+
+// called by https when the request is made.
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
+}
+
+// called by https when the request is made.
+var callback = function(response) {
+  console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
+}
+
+var options = {
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
+};
